9ebe8b0cc94dd521445c9717925144c2d1b539d5
waterbean0143 <102143428+waterbean0143@users.noreply.github.com>
2025-06-10 16:34:46 +0900

    Update SIBOT_mk2.py

--- SIBOT_mk2.py
+++ SIBOT_mk2.py
@@ -820,7 +820,7 @@ with qa_tab:
             tracer = LangChainTracer(project_name="SIBOT_MK2")
         else:
             tracer = None
-            answer = None
+        answer = None
  
         try:
             status_placeholder.info("⏳ 답변 생성 중...")
